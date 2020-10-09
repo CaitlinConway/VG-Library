e59fccca8ddd9359b6761fefee9557196611bcce
Caitlin Conway <coolcac@gmail.com>
2020-10-09 10:26:51 -0500

    models added

--- flask_react_starter/starter_app/app/models.py
+++ flask_react_starter/starter_app/app/models.py
@@ -21,3 +21,52 @@ class User(db.Model):
       "lastName": self.lastName,
       "zipCode": self.zipCode
     }
+
+class Console(db.Model):
+  __tablename__ = "consoles"
+
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(255), nullable=False, unique=True)
+
+class Game(db.Model):
+  __tablename__ = "games"
+
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(255), nullable=False, unique=True)
+  consoleId = db.Column(db.Integer, db.ForeignKey("consoles.id"))
+  console = db.relationship("console")
+
+
+class Library(db.Model):
+  __tablename__ = "libraries"
+
+  id = db.Column(db.Integer, primary_key = True)
+  gameId = db.Column(db.Integer, db.ForeignKey("games.id"))
+  game = db.relationship("game")
+  userId = db.Column(db.Integer, db.ForeignKey("users.id"))
+  user = db.relationship("user")
+
+
+class GameRequest(db.Model):
+  __tablename__ = "gameRequests"
+
+  id = db.Column(db.Integer, primary_key = True)
+  gameId = db.Column(db.Integer, db.ForeignKey("games.id"))
+  game = db.relationship("game")
+  userLibraryId = db.Column(db.Integer, db.ForeignKey("libraries.id"))
+  userLibrary = db.relationship("library")
+  userRequestId = db.Column(db.Integer, db.ForeignKey("users.id"))
+  userRequest = db.relationship("user")
+  requestStatus = db.Column(db.String(50))
+
+
+class GameReview(db.Model):
+  __tablename__ = "gameReviews"
+
+  id = db.Column(db.Integer, primary_key = True)
+  gameId = db.Column(db.Integer, db.ForeignKey("games.id"), nullable = False)
+  game = db.relationship("game")
+  userId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+  user = db.relationship("user")
+  rating = db.Column(db.Integer, nullable= False)
+  comments = db.Column(db.String(255))
