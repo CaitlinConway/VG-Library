ea48c73c87d75408f2d35361998595472c988a26
Caitlin Conway <coolcac@gmail.com>
2020-10-09 09:47:29 -0500

    user model added

--- flask_react_starter/starter_app/app/models.py
+++ flask_react_starter/starter_app/app/models.py
@@ -8,10 +8,16 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(40), nullable = False, unique = True)
   email = db.Column(db.String(255), nullable = False, unique = True)
+  firstName = db.Column(db.String(255), nullable= False)
+  lastName = db.Column(db.String(255), nullable= False)
+  zipCode = db.Column(db.Integer, nullable= False)
 
   def to_dict(self):
     return {
       "id": self.id,
       "username": self.username,
-      "email": self.email
+      "email": self.email,
+      "firstName": self.firstName,
+      "lastName": self.lastName,
+      "zipCode": self.zipCode
     }
