f9f975fb5c73cfd32c7aaee24387d0c04eb2329b
Caitlin Conway <coolcac@gmail.com>
2020-10-15 17:46:30 -0500

    position nav components

--- flask_react_starter/client/src/components/Homepage.js
+++ flask_react_starter/client/src/components/Homepage.js
@@ -5,21 +5,6 @@ import ConsoleFeed from "./ConsoleFeed";
 import { getAllConsoles } from "../store/gameReducer.js";
 
 class Homepage extends React.Component {
-  // const [consoles, setConsoles] = useState({});
-  // const dispatch = useDispatch();
-  // useEffect(() =>{
-  //   const loadConsoles = async () => {
-  //   const res = await fetch('/api/games/consoles');
-  //   if (res.ok){
-  //     res.data = await res.json();
-  //     dispatch(getConsoles(res.data['consoles']))
-  //   }
-  //   setConsoles(res.data['consoles']);
-  // }
-  // loadConsoles()
-  // }, [])
-
-  // if (consoles) return null;
   componentDidMount() {
     this.props.getAllConsoles();
   }
